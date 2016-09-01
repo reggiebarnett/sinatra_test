39ed2ff3a3f4414058e1e2180810ce45bcc47cd9
reggiebarnett <reggiedbarnett@gmail.com>
2016-09-01 15:30:33 -0400

    added color change

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,28 +2,35 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET = rand(100)
+color = 'white'
+
 def check_guess(guess)
 	if guess == SECRET
 		message = "You got it right!"
+		color = 'green'
 	elsif guess < SECRET
 		message = "Too low!"
+		color = '#FF8F91'
 		if guess < SECRET - 5
 			message = "Way too low!"
+			color = 'red'
 		end
 	elsif guess > SECRET
 		message = "Too high!"
+		color = '#FF8F91'
 		if guess > SECRET + 5
 			message = "Way too high!"
+			color = 'red'
 		end
 	else
 		message = "Please enter a number between 0 and 100"
 	end
 
-	message
+	return [message,color]
 end
 
 get '/' do
 	guess = params['guess'].to_i
-	message = check_guess(guess)
-	erb :index, :locals => {:secret => SECRET, :message => message}
+	mc = check_guess(guess)
+	erb :index, :locals => {:secret => SECRET, :message => mc[0], :color => mc[1]}
 end
